191b309eab8cf53cedb38f2713b9cb93612d6d75
RoboAI <robo.ai@outlook.com>
2019-09-25 00:26:28 +0100

    usual updates

---
+++ 3D Game Example/Assets/ButtonHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+
+    public void SetText(string text)
+    {
+        Debug.Log("set text");
+        //TextMesh textButton = transform.Find("ButtonTopScores").GetComponent<TextMesh>();
+        //textButton.text = text;
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        Debug.Log("ChangeScene");
+        SceneManager.LoadScene(sceneName);
+    }
+}
